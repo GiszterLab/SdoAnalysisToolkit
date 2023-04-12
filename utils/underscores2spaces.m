c9e1548f9d3e6bcf6e090c514b0e75b523d4d547
Trevor Smith <130182072+TrevorShawnSmith@users.noreply.github.com>
2023-04-12 18:53:24 -0400

    Delete utils directory

--- utils/underscores2spaces.m
+++
@@ -1,9 +0,0 @@
-function [stringOut] = underscores2spaces(stringIn)
-%// for conforming names w/ underscores
-strCell = strsplit(stringIn, '_'); 
-stringOut = strCell{1}; 
-for p=2:length(strCell) 
-    stringOut = strcat(stringOut, " ", strCell{p}); 
-end
-end
-
