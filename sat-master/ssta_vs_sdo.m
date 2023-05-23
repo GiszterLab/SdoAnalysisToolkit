2539c8152999c691e786cdc64e5291f9dcf6a587
Trevor Smith <130182072+TrevorShawnSmith@users.noreply.github.com>
2023-05-23 16:23:59 -0400

    Add files via upload

--- sat-master/ssta_vs_sdo.m
+++ sat-master/ssta_vs_sdo.m
@@ -4,8 +4,23 @@
 % spike-triggered averages to demonstrate how averaging results in a
 % less-accurate prediction of future signal behavior. 
 
-% Trevor S. Smith, 2022
+%_______________________________________
+% Copyright (C) 2023 Trevor S. Smith
 % Drexel University College of Medicine
+%
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+% 
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <https://www.gnu.org/licenses/>.
+%__________________________________________
 
 
 %% HEADER VARS
