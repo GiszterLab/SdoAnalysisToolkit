89f5b2c8a5b8c60e855c4e10f88c9280cd2bb52e
Phone Myat Kyaw <pmyatkyaw2018@gmail.com>
2024-09-11 14:31:53 -0400

    Transform Trevor's cells To Phone's cells
    -'getPKEMGCellFromTSEMGCell.m' opens a UI for input of Trevor's EMG cell.
     Then, it creates Phone's EMG cell for app input from Trevor's EMG cell
    
     -'getPKPPCellFromTSPPCell.m' opens a UI for input of Trevor's PP cell.
      Then, it creates Phone's PP cell for app input from Trevor's PP cell

---
+++ sat-master/+SAT/+app_input/getPKEMGCellFromTSEMGCell.m
@@ -0,0 +1,42 @@
+% Name: Phone Kyaw
+% Date: 09/10/2024
+
+function [pk_xt_cell, xtdc] = getPKEMGCellFromTSEMGCell()
+% Description:   Get Phone's xt_cell for the app from Trevor's xt_cell
+% Parameters:    None
+% Return Values: pk_xt_cell - Phone's xt_cell
+%                xtdc - xtDataCell
+
+arguments(Output)
+    pk_xt_cell
+    xtdc
+end
+   
+    % Get the filepath for '.mat' file
+    [file_name, path] = uigetfile({'*.mat', '.mat Files'}, 'MultiSelect','off');
+    file_path = fullfile(path, file_name);
+
+    % We only want one variable inside the '.mat' file
+    if length(whos('-file', file_path)) > 1
+        error('There is more than one file')
+    end
+    
+    % Get Trevor's xt_cell and import it insdie xtDataCell
+    ts_xt_cell = load(file_path);
+    field_name = fieldnames(ts_xt_cell);
+    ts_xt_cell = ts_xt_cell.(field_name{1});
+    xtdc = xtDataCell();
+    xtdc.import(ts_xt_cell);
+    
+    % Initialize PK xt_cell
+    pk_xt_cell = cell(length(ts_xt_cell), 2);
+    
+    for i = 1 : length(ts_xt_cell)
+        % Get raw data
+        tensor = xtdc.getTensor(1:xtdc.nChannels, i, 'DATAFIELD', 'raw')';
+        pk_xt_cell{i,2} = tensor;
+    end
+
+    % Get the channels' name and sampling rate
+    pk_xt_cell{1,1} = xtdc.sensor;
+end
